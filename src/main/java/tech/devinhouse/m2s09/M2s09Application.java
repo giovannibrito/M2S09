69f8af0983fc6f28b839f9b4ed57296f535678d5
giovannibrito <giovannileonibrito@bol.com.br>
2023-04-10 12:07:21 -0300

    Adiciona exercicio 5

---
+++ src/main/java/tech/devinhouse/m2s09/M2s09Application.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.m2s09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s09Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devinhouse/m2s09/M2s09ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.m2s09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
